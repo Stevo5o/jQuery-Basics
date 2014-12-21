d7d8ba34a51ccc321e72f6f67c932964e9d366a4
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-21 12:15:12 +0000

    basic JQuery event and effect

--- myjqscript.js
+++ myjqscript.js
@@ -2,5 +2,10 @@
   (function($) {     
      
      $('h1').addClass('red');
+     // button event
+     $('button').click(function() {
+     //	$('h1').html('Here is some new text');
+     	$('h1').slideToggle(200);
+     });
      
   })(jQuery);
