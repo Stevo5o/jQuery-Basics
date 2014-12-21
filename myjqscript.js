f1c228d269c6f8a22dff253564d8d13efc399830
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-21 12:30:10 +0000

    basic JQuery event and effect

--- myjqscript.js
+++ myjqscript.js
@@ -1,11 +1,14 @@
 /* @ Stephen O'Connor */
   (function($) {     
-     
+     // api.jquery.com/category/effects/
      $('h1').addClass('red');
      // button event
      $('button').click(function() {
      //	$('h1').html('Here is some new text');
-     	$('h1').slideToggle(200);
+     	$('h1').html('Change Text').slideToggle(200);
      });
      
+             alert('Say "Hi" O\'Connor');
+             alert("Say 'Hi'");
+             alert("Say 'Hi' O'Connor");
   })(jQuery);
