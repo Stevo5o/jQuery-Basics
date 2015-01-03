9478c1e5ead0a7405c429fd868613c6806d00902
Stephen <Stephen@O-Connor-ToshL>
2015-01-03 22:15:27 +0000

    jQuery $ not working?

--- myjqscript.js
+++ myjqscript.js
@@ -1,10 +1,13 @@
 /* @ Stephen O'Connor */
-  (function($) {     
-     // api.jquery.com/category/effects/
-     $('h1').addClass('red');
-     // button event
-     $('button').click(function() {
-     //	$('h1').html('Here is some new text');
-     	$('h1').html('Change Text').slideToggle(200);
-     });    
-  })(jQuery);
+(function ($) {
+   // api.jquery.com/category/effects/
+   $('h1').addClass('red');
+   // button event
+   $('button').click(function () {
+      //	$('h1').html('Here is some new text');
+      $('h1').html('Change Text').slideToggle(200);
+      
+      // why is dollar not working
+      jQuery('ul li').addClass('emphasis');
+   });
+})(jQuery);
