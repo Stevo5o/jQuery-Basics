a0cefd006a0d6e4b2249addd959f700fc926e69e
theWhiteFox <ste@thewhitefox.ninja>
2018-02-28 17:00:37 +0000

    :moon: clean up :sunny:

---
+++ script.js
@@ -0,0 +1,22 @@
+/* basic jQuery */
+(function () {    
+   
+    // button event
+    $('#btn-hide-text').click(function () {   
+        $('h1').addClass('red');   
+        $('h1').html('Hide me').slideToggle(200);
+        $('body').addClass('black');
+        $("#btn-hide-text").text(function(i, text){
+                return text === "PUSH ME" ? "DON'T PUSH ME" : "PUSH ME";
+        });
+    });
+ 
+    // change color
+    $('#btn-color-text').click(function () { 
+        $('ul.color-this>li').addClass('emphasis');        
+        $('#content>p').addClass('blue');    
+    });     
+
+  console.log("Keep on basic jQuery!");
+       
+})();
